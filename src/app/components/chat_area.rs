f5e6705532eb440abab6dd15f044ff5336d51190
z-shaffer <zshaffer@protonmail.com>
2024-02-20 00:47:39 +0100

    simplifying css for chat area

--- src/app/components/chat_area.rs
+++ src/app/components/chat_area.rs
@@ -1,40 +1,20 @@
-use leptos::*;
-
-use crate::model::conversation::Conversation;
-
-const USER_MESSAGE_DARK_MODE_COLORS: &str  = "bg-blue-500 text-white";
-const USER_MESSAGE_LIGHT_MODE_COLORS: &str  = "bg-blue-700 text-white";
-const USER_MESSAGE_CLASS: &str = "max-w-md p-4 mb-5 rounded-lg self-end";
-
-const MODEL_MESSAGE_LIGHT_MODE_COLORS: &str = "bg-gray-200 text-black";
-const MODEL_MESSAGE_DARK_MODE_COLORS: &str = "bg-zinc-700 text-white";
-const MODEL_MESSAGE_CLASS: &str = "max-w-md p-4 mb-5 rounded-lg self-start";
-
-const CHAT_AREA_CLASS: &str = "h-screen pb-24 w-full flex flex-col overflow-y-auto p-5";
-const CHAT_AREA_LIGHT_MODE_COLORS: &str = "border-gray-300 bg-gray-100";
-const CHAT_AREA_DARK_MODE_COLORS: &str = "border-zinc-700 bg-zinc-900";
+use leptos::{*, html::Input};
 
 #[component]
-pub fn ChatArea(conversation: ReadSignal<Conversation>) -> impl IntoView {
-    let input_ref = create_node_ref::<Div>();
-    create_effect(move |_| {
-        conversation.get();
-        if let Some(div) = chat_div_ref.get() {
-            div.set_scroll_top(div.scroll_height());
-        }
-    });
-
-    view! {
-        <div class={chat_area_class.get()} node_ref=chat_div_ref>
-            {move || conversation.get().messages.iter().map(move |message| {
-                let class_str = if message.user { USER_MESSAGE_CLASS } else { MODEL_MESSAGE_CLASS };
-                view! {
-                    <div class={class_str}>
-                    {message.text.clone()}
-                    </div>
-                }
-            }).collect::<Vec<_>>()
-        }
+pub fn TypeArea(send: Action<String, Result<String, ServerFnError>>) -> impl IntoView {
+    let input_ref = create_node_ref::<Input>();
+    view!{
+        <div class="h-24 w-full fixed bottom-0 flex justify-center items-center p-5 bg-white border-t border-gray-300">
+           <form class="w-full flex justify-center items-center gap-4" on:submit=move |ev| {
+                ev.prevent_default();
+                let input = input_ref.get().expect("input to exist");
+                send.dispatch(input.value());
+                input.set_value("");
+           }
+           >
+                <input class="w-2/3 p-4 border border-gray-300 rounded-full" type="text" placeholder="Enter your prompt" node_ref=input_ref/>
+                <input class="h-full p-4 bg-blue-500 text-white rounded-full cursor-pointer" type="submit"/>
+           </form>
         </div>
     }
 }
